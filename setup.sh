f24db6e6353f9384bafdf85709d54000b49b9bad
tianyawy <tianya_wy@hotmail.com>
2016-08-03 14:02:47 +0800

    [victoryw] setup the env

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+brew install nvm
+pushd $(pwd)
+. $(brew --prefix nvm)/nvm.sh
+popd
+
+nvm install node #lastest node
+nvm use node
+
+npm install -g es-checker #the lastest system runtime to support the es features
+
+npm install -g babel-cli
+
+
+if [ ! -f ./package.json ]
+then
+	npm init -y
+fi
+
+npm install --save babel-preset-es2015
